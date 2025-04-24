2eb1f62fa3c99228273d28166c2b91e9f4f8f14c
Mateus-tech <mateus.queiroz.ti.mq@gmail.com>
2025-04-23 23:47:48 -0300

    Implementa a aplicação principal do MangaSound, permitindo adicionar músicas, gerenciar listas de reprodução e controlar a execução de playlists via terminal. e Define a classe Musica, que representa uma música com nome, artista, caminho do arquivo e duração em segundos

---
+++ manga-sound-player/src/main/java/model/Musica.java
@@ -0,0 +1,37 @@
+package drive.rede.com.br.model;
+
+public class Musica {
+    private String nome;
+    private String artista;
+    private String caminho;
+    private int duracao; // duração em segundos
+
+    public Musica(String nome, String artista, String caminho, int duracao) {
+        this.nome = nome;
+        this.artista = artista;
+        this.caminho = caminho;
+        this.duracao = duracao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public String getCaminho() {
+        return caminho;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+
+    @Override
+    public String toString() {
+        return nome + " - " + artista + " (" +duracao+ ")";
+    }
+}
